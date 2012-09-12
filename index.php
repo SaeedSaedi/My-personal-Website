10560f187d5e15c65a8af64960d77d4b031dce3c
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-09-12 23:24:33 +0430

    style of footer

--- index.php
+++ index.php
@@ -91,10 +91,24 @@
 	</div>
 	<div class="container-footer">
 		<div class="footer">
-			<div class="facebook"></div>
-			<div class="twitter"></div>
-			<div class="info"></div>
-			<div class="copyright"></div>
+			<div class="facebook">
+				<a href="#">
+					<img src="./image/facebook.png" alt="">
+					<p>Facebook</p>
+				</a>
+			</div>
+			<div class="twitter">
+				<a href="#">
+					<img src="./image/twitter.png" alt="">
+					<p>Twitter</p>
+				</a>
+			</div>
+			<div class="info">
+				<p>this webpage is desgin for group of developer that style the internet keep and support</p>
+			</div>
+			<div class="copyright">
+				<p>All right reserved By slash 2012</p>
+			</div>
 		</div>
 	</div>
 	

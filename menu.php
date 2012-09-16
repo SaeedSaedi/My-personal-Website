396023a92df0bcd6f70272d6667505b14120a064
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-09-17 02:15:38 +0430

    change menu href

--- menu.php
+++ menu.php
@@ -1,8 +1,8 @@
 <div class="container-menu">
 	<div class="menu">
 		<ul>
-			<li><a href="#" class='active'>HOME</a></li>
-			<li><a href="#">PROJECT</a></li>
+			<li><a href="./index.php" class='active'>HOME</a></li>
+			<li><a href="./index-project.php">PROJECT</a></li>
 			<li><a href="#">CONTACT US</a></li>
 			<li><a href="#">ABOUT US</a></li>
 		</ul>

9a955dadec6f28e471989f58ef051e79ba5230b5
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-09-17 03:22:12 +0430

    project content structure and style of it

--- index-project.php
+++ index-project.php
@@ -3,6 +3,6 @@
 include 'header.php';
 include 'banner-head.php';
 include 'menu.php';
-
+include 'content-project.php';
 include 'footer.php';
 ?>
--- index.php
+++ index.php
@@ -4,9 +4,7 @@ include 'header.php';
 include 'banner-head.php';
 include 'menu.php';
 include 'slideshow.php';
-include 'banner-center.php';
 include 'content.php';
-include 'banner-foot.php';
 include 'footer.php';
 
 ?>

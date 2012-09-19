11a3a3e6e7aadb966e58347d3dba58138a0eac39
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-09-20 02:35:18 +0330

    change href of menu

--- menu.php
+++ menu.php
@@ -1,9 +1,9 @@
 <div class="container-menu">
 	<div class="menu">
 		<ul>
-			<li><a href="./index.php" class='active'>HOME</a></li>
+			<li><a href="./index.php">HOME</a></li>
 			<li><a href="./index-project.php">PROJECT</a></li>
-			<li><a href="#">CONTACT US</a></li>
+			<li><a href="./index-contact.php">CONTACT US</a></li>
 			<li><a href="#">ABOUT US</a></li>
 		</ul>
 	</div>

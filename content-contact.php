e52d41611b75e3967d0d5d639b24d682ca059bc5
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-09-20 17:15:44 +0330

    start desgin of contact form

---
+++ content-contact.php
@@ -0,0 +1,16 @@
+<div class="container-contact">
+	<div class="contact">
+		<div class="form">
+			<form action="" method="" class="contact">
+				<p>Name</p>
+				<input type="text" />
+				<p>E-mail</p>
+				<input type="text" />
+				<p>Subject</p>
+				<input type="text" />
+				<p>Message</p>
+				<textarea name="" id="" cols="30" rows="10"></textarea>
+			</form>
+		</div>
+	</div>
+</div>

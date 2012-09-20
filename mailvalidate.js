3f4f5af6542560397665fa64e9af85f4d19e2b3f
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-09-21 02:54:10 +0330

    complate txt validate and mail validate

--- mailvalidate.js
+++ mailvalidate.js
@@ -2,6 +2,12 @@ function validateText(str,len){
 	return str.length >= len;
 };
 
+function validateEmail(str){
+	var emailPattern = /^[a-z0-9+_%.-]+@(?:[a-z0-9-]+\.)+[a-z]{2,6}$/i ;
+
+	return emailPattern.test(str);
+};
+
 $(function(){
 	$('form.contact').submit(function(){
 		var target, error = false;
@@ -12,10 +18,31 @@ $(function(){
 		}else{
 			target.removeClass('ok').addClass('err');
 			err = true;
-		}
+		};
 
+		target = $('input#subject');
+		if( validateText(target.val(),5) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		};
 
+		target = $('textarea#message');
+		if( validateText(target.val(),10) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		};
 
+		target = $('input#mail');
+		if( validateEmail(target.val()) ){
+			target.removeClass('err').addClass('ok');
+		}else{
+			target.removeClass('ok').addClass('err');
+			err = true;
+		};
 
 		return !err;
 	});

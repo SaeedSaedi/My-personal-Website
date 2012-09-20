77e4ad7626734365978fa0478eb5fa6e1e5178b7
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-09-20 18:26:35 +0330

    contact form design

--- content-contact.php
+++ content-contact.php
@@ -3,13 +3,15 @@
 		<div class="form">
 			<form action="" method="" class="contact">
 				<p>Name</p>
-				<input type="text" />
+				<input name="name" id="name" placeholder="Name"  type="text" />
 				<p>E-mail</p>
-				<input type="text" />
+				<input name="mail" id="mail" placeholder="E-mail" type="text" />
 				<p>Subject</p>
-				<input type="text" />
+				<input name="subject" id="subject" placeholder="Subject" type="text" />
 				<p>Message</p>
-				<textarea name="" id="" cols="30" rows="10"></textarea>
+				<textarea  name="message" id="message" placeholder="Message" cols="30" rows="8"></textarea>
+				<input id="submit" name="submit" value="Send" type="submit" />
+				<input id="reset" name="reset" type="reset" />
 			</form>
 		</div>
 	</div>

2cbcb8c413edc1301d80e9541189ff6dfc397fb1
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-09-21 20:54:26 +0330

    add coment

--- aboutus.js
+++ aboutus.js
@@ -1,7 +1,10 @@
 $(function(){
+	// Active menu for about us
 	var menu = $('div.menu a');
 	menu.eq(3).click(function(){
 		menu.eq(2).removeClass('active');
 		$(this).addClass('active');
 	});
+	
+
 });

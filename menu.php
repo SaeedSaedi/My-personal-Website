edd3c25a0982c8a11179deaa82ae344973939415
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-09-21 20:32:51 +0330

    add active menu in each page

--- menu.php
+++ menu.php
@@ -1,9 +1,9 @@
 <div class="container-menu">
 	<div class="menu">
 		<ul>
-			<li><a href="./index.php">HOME</a></li>
-			<li><a href="./index-project.php">PROJECT</a></li>
-			<li><a href="./index-contact.php">CONTACT US</a></li>
+			<li><a class="<?php if($page == 'home') echo 'active'?>" href="./index.php">HOME</a></li>
+			<li><a class="<?php if($page == 'project') echo 'active'?>" href="./index-project.php">PROJECT</a></li>
+			<li><a class="<?php if($page == 'contact') echo 'active'?>" href="./index-contact.php">CONTACT US</a></li>
 			<?php 
 				if ($page == 'contact') {
 					echo "<li><a href='#''>ABOUT US</a></li>";

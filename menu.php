a28ff6b748685429a0778aef4787c890116d33cd
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-09-21 20:22:29 +0330

    echo about us menu just in contact page

--- menu.php
+++ menu.php
@@ -4,7 +4,11 @@
 			<li><a href="./index.php">HOME</a></li>
 			<li><a href="./index-project.php">PROJECT</a></li>
 			<li><a href="./index-contact.php">CONTACT US</a></li>
-			<li><a href="#">ABOUT US</a></li>
+			<?php 
+				if ($page == 'contact') {
+					echo "<li><a href='#''>ABOUT US</a></li>";
+				};
+			?>
 		</ul>
 	</div>
 </div>

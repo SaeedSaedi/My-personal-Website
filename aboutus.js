b1be78174ea4d899df05d5e0d8c5f2b311217ec4
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-09-21 23:59:10 +0330

    about us hidden style and animation

--- aboutus.js
+++ aboutus.js
@@ -6,5 +6,30 @@ $(function(){
 		$(this).addClass('active');
 	});
 	
+	//About us page Animation
+
+	var title = $('div.aboutus > p'),
+		maintxt = $('div.information p'),
+		images = $('div.aboutimg img'),
+		i = 0;
+
+		menu.eq(3).click(function(){
+			setTimeout(titlewidth,1000);
+		});
+
+		titlewidth = function(){
+			title.animate({width:'200px'});
+			setTimeout(mainheight,1000);
+		};
+		mainheight = function(){
+			maintxt.animate({height:'inherit'},2000);
+			for(i=0;i<4;i++){
+				setTimeout(img(i),2000);
+			}
+		};
+
+		img = function(i){
+			images.eq(i).animate({marginTop:'0px'},1000+500*i);
+		}
 
 });
